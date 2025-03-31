8d3e5b3f3ddb4343502cf982b90686cda7cbfe41
Ashok Rajendran <69075090+ashok-rajendran@users.noreply.github.com>
2025-04-01 01:49:10 +0530

    Create EnKrypt_Fernet.py

---
+++ udf/EnKrypt_Fernet.py
@@ -0,0 +1,16 @@
+create or replace function EnKrypt_AES(input_text string)
+returns string
+language python
+runtime_version = '3.8'
+handler = 'tokenize'
+packages = ('cryptography')
+as 
+$$
+from cryptography.fernet import Fernet
+
+encryption_key = Fernet.generate_key()
+cipher = Fernet(encryption_key)
+
+def tokenize(input_text):
+  return cipher.encrypt(input_text.encode()).decode()
+$$
